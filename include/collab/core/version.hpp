9465718993eb60e2b502bd5816e3f25d307b4ccf
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-11 10:20:55 -0700

    Blank slate

--- include/collab/core/version.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "collab/version.hpp"
-
-namespace collab::core {
-
-inline constexpr collab::Version version{0, 5, 0};
-
-}  // namespace collab::core
--- src/core.cpp
+++
@@ -1,3 +0,0 @@
-// Placeholder so the static library has a translation unit.
-// collab-core is mostly headers right now but will grow.
-#include "collab/version.hpp"
