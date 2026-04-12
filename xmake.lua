982092ddff19793a582c1998d691a2597b82ef0a
Mrowr Purr <mrowr.purr@gmail.com>
2026-04-12 13:47:44 -0700

    Remove no longer needed policy

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,6 @@ set_defaultmode("release")
 set_languages("c++23")
 set_policy("build.c++.modules", true)
 
--- xmake forces -D_GLIBCXX_USE_CXX11_ABI=0 for GCC < 15 when modules are on
--- (workaround for xmake#2716/#3855). That mismatches the default-new-ABI
--- Catch2 package and breaks the link. Opt back into the new ABI.
-set_policy("build.c++.modules.gcc.cxx11abi", true)
-
 option("build_tests")
     set_default(true)
     set_showmenu(true)
